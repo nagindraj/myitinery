01a731ffbc30bfecb235d9b95bda571d0d811e2a
Nagindra <nagi@Nagindras-MacBook-Air.local>
2019-06-10 15:57:55 +0200

    connect to database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const router = require('./routes');
+const connectToMongo = require('./database');
 
+//connect to database
+connectToMongo();
 // apply the routes to our application
 app.use('/', router);
  
